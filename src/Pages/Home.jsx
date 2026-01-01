701ab5e678dca99c51ad47a5673ecd72f2e897e5
asad3473 <asadsoomro0311@gmail.com>
2026-01-01 18:25:00 +0500

    set home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import HomeActivity from '../Components/Home/HomeActivity'
-import HomeTrips from '../components/home/HomeTrips'
-import HomeHeader from '../components/home/HomeHeader'
-import Services from '../components/home/ExploreOurFacilities'
+import HomeTrips from '../components/Home/HomeTrips'
+import HomeHeader from '../components/Home/HomeHeader'
+import Services from '../components/Home/ExploreOurFacilities'
 
 export default function Home() {
   return (
