5ff1859563ccdbfb8f0d8c49c96537f100e75a7f
asad3473 <asadsoomro0311@gmail.com>
2026-01-01 18:18:06 +0500

    home activity

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Homeactivity from '../components/home/Homeactivity'
+import HomeActivity from '../Components/Home/HomeActivity'
 import HomeTrips from '../components/home/HomeTrips'
 import HomeHeader from '../components/home/HomeHeader'
 import Services from '../components/home/ExploreOurFacilities'
@@ -8,10 +8,10 @@ import Services from '../components/home/ExploreOurFacilities'
 export default function Home() {
   return (
     <div className='mt-10'>
-      <HomeHeader/>
-      <Homeactivity/>
-      <HomeTrips/>
-      <Services/>
+      <HomeHeader />
+      <HomeActivity />
+      <HomeTrips />
+      <Services />
     </div>
   )
 }
