22a3fec41daacda27314118d2f3eeb9c6b13b99a
asad3473 <asadsoomro0311@gmail.com>
2026-01-02 12:29:33 +0500

    Fix import casing for HomeActivity component in MainHome.jsx

--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HomeHeader from '../components/home/HomeHeader.jsx'
-import HomeActivity from '../components/Home/Homeactivity.jsx'
+import HomeActivity from '../components/Home/HomeActivity.jsx'
 import HomeTrips from '../components/Home/HomeTrips.jsx'
 import Services from '../components/home/Services.jsx'
 
